e11fbe89a9a1e2a136c1f379705e4bd2610d59b2
ashulikesneeze <93233404+boram1219@users.noreply.github.com>
2022-03-17 11:59:15 +0900

    bbhouse

--- SQL/bbhouse_middle_category.sql
+++ SQL/bbhouse_middle_category.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `bbhouse` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bbhouse`;
 -- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
 --
 -- Host: localhost    Database: bbhouse
@@ -50,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-17 10:26:18
+-- Dump completed on 2022-03-17 11:58:02
